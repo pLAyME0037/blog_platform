707b75e0c1325c58d914663e612bcde9e63420e6
Sok Mean <mean77929@gamil.com>
2025-12-17 17:24:54 +0700

    add seeder for [user, post, comment]

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class PostFactory extends Factory
+{
+    public function definition(): array
+    {
+        $title = $this->faker->sentence;
+        return [
+            'user_id' => User::factory(),
+            'title' => $title,
+            'slug' => Str::slug($title),
+            'content' => $this->faker->paragraphs(3, true),
+            'published_at' => $this->faker->boolean(80) ? now() : null, // 80% chance of being published
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,40 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
+use App\Models\Post;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
+        // 1. [CREATION] We define the variable '$mainUser' here.
+        $mainUser = User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        // 2. We create 10 other random users
+        $users = User::factory(10)->create();
+
+        // 3. [USAGE] We use the '$mainUser' variable we created in Step 1.
+        // We pass the ID into the create() array to link these posts to our Test User.
+        Post::factory(5)->create([
+            'user_id' => $mainUser->id, 
+        ]);
+
+        // 4. Create random posts for the other users
+        $users->each(function ($user) use ($users) {
+            Post::factory(3)->create([
+                'user_id' => $user->id,
+            ])->each(function ($post) use ($users) {
+                Comment::factory(rand(1, 3))->create([
+                    'post_id' => $post->id,
+                    'user_id' => $users->random()->id,
+                ]);
+            });
+        });
     }
 }
